ac085cb99a7ba80135d1ee222b568450e65a93be
Kralickova, Eva <eva.kralickova@oracle.com>
2022-11-29 20:18:16 +0100

    ukol 2 a 3

--- cafeLoraApp/src/index.js
+++ cafeLoraApp/src/index.js
@@ -1,3 +1,26 @@
 import './style.css';
+import { Banner } from './conponents/Banner/index.js';
+import { Menu } from './conponents/Menu/index.js';
+import { Gallery } from './conponents/Gallery/index.js';
+import { Contact } from './conponents/Contact/index.js';
+import { Header } from './conponents/Header/index.js';
+import { Footer } from './conponents/Footer/index.js';
 
-console.log('funguju!');
+const pageElement = document.createElement('div');
+pageElement.classList.add('page');
+
+const main = document.createElement('main');
+main.append(
+  Banner(),
+  Menu(),
+  Gallery(),
+  Contact(),
+);
+
+pageElement.append(
+  Header(),
+  main,
+  Footer(),
+);
+
+document.querySelector('#app').append(pageElement);
